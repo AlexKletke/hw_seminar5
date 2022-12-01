226612a640114006c5fab395521dc923e38f41e1
AlexandrKletke <klete@mail.ru>
2022-12-01 22:35:58 +0300

    solution of the 34th example

---
+++ ex34/Program.cs
@@ -0,0 +1,31 @@
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+using static System.Console;
+
+int[] FillArray(int size)
+{
+    int[] array = new int[size];
+    for(int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(100, 1000);
+    return array;
+}
+int CountEvenNumbers(int[] array)
+{
+    int count = 0;
+    for(int j = 0; j < array.Length; j++)
+    {
+        if(array[j]%2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+WriteLine("enter size of array: ");
+int size = int.Parse(ReadLine());
+int[] array = FillArray(size);
+int count = CountEvenNumbers(array);
+WriteLine(String.Join(", ", array));
+WriteLine($"quantity of even numbers in the array = {count}");
+
